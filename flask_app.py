59245c3f03fab1938b7c6b22c395db11634f99d7
kang sungwan <kang.sungwan@example.com>
2026-08-09 21:50:53 +0900

    Add base layout and simple customer login page

--- flask_app.py
+++ flask_app.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello from Flask!'
+@app.route('/', methods=['GET', 'POST'])
+def login():
+    return render_template('login.html')
